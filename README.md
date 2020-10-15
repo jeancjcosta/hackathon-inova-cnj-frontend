15b75b09290a084db665c8495071a916c305483e
Jean <jean.costa@cnj.jus.br>
2020-10-15 00:42:59 -0300

    [feat] tela basica para mostrar gráficos usando vuejs

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# Inova CNJ Frontend
+# inova-cnj-frontend
 
-Projeto para construir uma interface web para visualização e demonstração das solução do desafio Inova CNJ 
+# Orientações
+## Instale node, npm e vue cli
+- https://nodejs.org/en/
+- https://cli.vuejs.org/guide/installation.html
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h3>Exemplos de Gráficos</h3>
+
+    <line-chart :data="chartData"></line-chart>
+
+    <button @click="updateChart">Atualizar Dados</button>
+
+    <pie-chart :data="[['Nokia', 150],['Sansung', 120],['Apple',200]]"></pie-chart>
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      chartData: {
+        '2020-05-13': 2,
+        '2020-05-15': 5,
+        '2020-05-17': 4
+      }
+    }
+  },
+
+  methods: {
+    updateChart() {
+      this.chartData = {
+        '2020-05-13': 7,
+        '2020-05-15': 9,
+        '2020-05-17': 16
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
