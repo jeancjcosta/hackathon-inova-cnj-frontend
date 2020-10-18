b3bd8adfdc41c68eab8861bc68a3d794f63ea59c
Jean <jean.costa@cnj.jus.br>
2020-10-18 20:40:06 -0300

    [feat] Separação em abas das funcionalidades

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
+
 <template>
   <div id="app">
-    <h3>Exemplos de Gráficos</h3>
+    <v-container>
+      <h1>Desafio 1 CNJ Inova - TEMPO E PRODUTIVIDADE</h1>
+      <v-col>
+        <v-row>
+          <v-btn
+            block
+            color="secondary"
+            depressed
+            elevation="1"
+            large
+            v-for='tab in tabs' :key='tab' @click='chooseTab(tab)'
+          >{{tab}}</v-btn>
 
-    <line-chart :data="chartData"></line-chart>
-
-    <button @click="updateChart">Atualizar Dados</button>
-
-    <pie-chart :data="[['Nokia', 150],['Sansung', 120],['Apple',200]]"></pie-chart>
+        </v-row>
+        <v-row>
+          <keep-alive>
+            <component :is="component"></component>
+          </keep-alive>
+        </v-row>
+      </v-col>
+    </v-container>
   </div>
 
 </template>
 
 <script>
-
+import axios from 'axios'
+//import Treeselect from '@riophae/vue-treeselect'
+  // import the styles
+//import '@riophae/vue-treeselect/dist/vue-treeselect.css'
 
 export default {
+  //components: { Treeselect },
+  
   name: 'App',
   data() {
     return {
+      component: 'bottle-neck',
+      tabs: ['Identificação de Gargalos', 'Desempenho da Serventia',  'Expectativa de Tempo de Tramitação'],
       chartData: {
         '2020-05-13': 2,
         '2020-05-15': 5,
         '2020-05-17': 4
-      }
+      },
+
+      info: null,
+      value: [],
+      baseUrl: 'http://localhost:8000',
+      options1: [ {
+        id: 'a',
+        label: 'a',
+        children: [ {
+          id: 'aa',
+          label: 'aa',
+        }, {
+          id: 'ab',
+          label: 'ab',
+        } ],
+      }, {
+        id: 'b',
+        label: 'b',
+      }, {
+        id: 'c',
+        label: 'c',
+      } ],
     }
   },
 
@@ -33,12 +76,31 @@ export default {
         '2020-05-15': 9,
         '2020-05-17': 16
       }
+    },
+
+    pegarDadosTeste() {
+      axios
+      .get(this.baseUrl + '/api/score')
+      .then(response => {
+        this.info = response.data
+        console.log(response.data)
+      })
+    },
+
+    chooseTab(tab) {
+      if (tab == 'Identificação de Gargalos') {
+        this.component = 'bottle-neck'
+      } else if (tab == 'Desempenho da Serventia') {
+        this.component = 'performance'
+      } else if (tab == 'Expectativa de Tempo de Tramitação') {
+        this.component = 'expected-process-duration'
+      }
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -46,5 +108,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: rgb(238, 232, 229);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,23 @@ import Vue from  'vue'
 import App from './App.vue'
 import Chartkick from 'vue-chartkick'
 import Chart from 'chart.js'
+import Vuetify from 'vuetify'
+import Highcharts from 'highcharts'
+import ExpectedProcessDuration from '@/components/ExpectedProcessDuration.vue'
+import BottleNeck from '@/components/BottleNeck.vue'
+import Performance from'@/components/Performance.vue'
 
+
+Vue.use(Vuetify)
+Vue.use(Chartkick)
 Vue.use(Chartkick.use(Chart))
+Vue.use(Chartkick.use(Highcharts))
 
 Vue.config.productionTip = false
+
+Vue.component('expected-process-duration', ExpectedProcessDuration)
+Vue.component('bottle-neck', BottleNeck)
+Vue.component('performance', Performance)
 
 new Vue({
     render: h => h(App),
